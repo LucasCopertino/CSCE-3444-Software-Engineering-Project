68af915a066ce98a270cf905528bdd69429d1aa8
olufemi-olumaiyegun <femimayegun@gmail.com>
2021-03-19 15:19:15 -0500

    Created the orders app and database table

--- src/restaurant/orders/models.py
+++ src/restaurant/orders/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from menu.models import Item
 # Create your models here.
+
+STATUS_CHOICES = [('Created', 'created'), ('In progress', 'in progress'), ('Finished','finished'),('Delivered','delivered')]
+class order(models.Model):
+    table_num = models.IntegerField( null=False, default=0)
+    time = models.DateTimeField()
+    total_items = models.IntegerField(default=1)
+    Item = models.ManyToManyField(Item)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES)
+    order_id = models.IntegerField(null=True)
+    def __str__(self):
+        return self.order_id
--- src/restaurant/restaurant/settings.py
+++ src/restaurant/restaurant/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'menu', #our update - install the app we created from the terminal 
+    'orders',
 ]
 
 MIDDLEWARE = [
