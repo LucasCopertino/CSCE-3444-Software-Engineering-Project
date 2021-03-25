092dbf7deb3f0288be0a6f43413144fc0e965acb
olufemi-olumaiyegun <femimayegun@gmail.com>
2021-03-25 11:06:09 -0500

    user registration added

--- src/restaurant/accounts/views.py
+++ src/restaurant/accounts/views.py
@@ -2,7 +2,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
