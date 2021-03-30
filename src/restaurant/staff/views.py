bed510e8c97aec032bfd75c6f9287bd15e5ccca3
olufemi-olumaiyegun <femimayegun@gmail.com>
2021-03-30 13:12:02 -0500

    kitchen queue now updated automatically whenever an order is made

--- src/restaurant/staff/views.py
+++ src/restaurant/staff/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
-
+from orders.models import order
 def waiter_home(request):
     return render(request, 'waiter_notif.html') #page for waiter notifications
 
 def kitchen_home(request):
-    return render(request, 'kitchen_queue.html') #page for kitchen queue
+    order_objs = order.objects.all()
+    order_items = []
+    for s in order_objs:
+       order_items.append(s.items.all())
+
+   
+    context = {
+        'orders':order_objs,
+        'items':order_items
+    }
+    return render(request, 'kitchen_queue.html', context) #page for kitchen queue
 
 def manager_home(request):
     return render(request, 'manager_home.html') #page for manager home
